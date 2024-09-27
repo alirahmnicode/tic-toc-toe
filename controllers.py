996174212fb2b0c80c93d934a48abfa249b6b036
alirahmanicode <alirahmanicode@gmail.com>
2024-09-27 13:04:17 +0330

    main socket server splited up into several controlers

---
+++ controllers.py
@@ -0,0 +1,72 @@
+import random
+from fastapi import WebSocket
+from connection import ConnectionManager
+from game import TicTacToeGame
+
+
+async def create_game(
+    game: TicTacToeGame, ws_manager: ConnectionManager, websocket: WebSocket
+):
+    payload = {
+        "method": "CreateGame",
+        "status": "ok",
+        "gameId": game._game_id,
+    }
+    await ws_manager.send(payload=payload, websocket=websocket)
+
+
+# join to a existing game
+async def join_game(
+    player, game: TicTacToeGame, ws_manager: ConnectionManager, websocket: WebSocket
+):
+    player.label = "O"
+    if game is not None:
+        game.add_player(player)
+        payload = {"method": "JoinToGame", "status": "ok"}
+        await ws_manager.send(payload=payload, websocket=websocket)
+    else:
+        print("game is not exist")
+
+
+# join to a random game
+async def join_random_game(
+    game: TicTacToeGame, player, ws_manager: ConnectionManager, websocket: WebSocket
+):
+    player.label = "O"
+    payload = {
+        "method": "JoinToRandomGame",
+    }
+    if game:
+        game.add_player(player)
+        payload["message"] = "You have joined the game successfully!"
+        payload["status"] = "ok"
+        payload["game_id"] = game._game_id
+    else:
+        payload["message"] = "There is no any game!"
+        payload["status"] = "NotFound"
+    print("4 ", payload)
+    await ws_manager.send(payload=payload, websocket=websocket)
+
+
+# handle move
+async def handle_move(
+    row: int, col: int, game: TicTacToeGame, ws_manager: ConnectionManager
+):
+    payload = {"method": "HandleMove", "status": "ok", "has_winner": False}
+    if row is not None and col is not None:
+        if game:
+            game.play(row, col)
+            # send this move to the next player
+            # in play() method the toggle_player() method is called
+            move = f"{row}_{col}"
+            payload["message"] = move
+            await ws_manager.send(payload=payload, user_id=game.current_player.id)
+            # if the game hss winner send winner compos to both players
+            if game.has_winner():
+                for player in game.players:
+                    payload["winner_combo"] = game.winner_combo
+                    payload["has_winner"] = True
+                    payload["winner_player"] = game._winner_player
+                    await ws_manager.send(payload=payload, user_id=player.id)
+        else:
+            raise Exception("game dose not exist")
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import random
 import uuid
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from game import Player, TicTacToeGame
+from connection import ConnectionManager
+from controllers import create_game, handle_move, join_game, join_random_game
 
 
 app = FastAPI()
+manager = ConnectionManager()
 
 # settings //////////////////////////////////////////////////////////////////////////
 app.add_middleware(
@@ -30,69 +33,31 @@ players = {}
 
 @app.websocket("/ws/game")
 async def game(websocket: WebSocket):
-    await websocket.accept()
     player = Player(id=str(uuid.uuid4()), label="X", color="green")
-    game: TicTacToeGame = None
-    players[player.id] = websocket
+    await manager.connect(websocket, player.id)
 
-    while True:
-        data = await websocket.receive_json()
-        if data["method"] == "CreateGame":
-            game = TicTacToeGame(players=(player,))
-            games[game._game_id] = game
-            payload = {"method": "CreateGame", "status": "ok", "message": game._game_id}
-            await websocket.send_json(payload)
-        elif data["method"] == "JoinToGame":
-            player.label = "O"
-            game_id = data["game_id"]
-            game: TicTacToeGame = games.get(game_id)
-            if game is not None:
-                game.add_player(player)
-                payload = {"method": "JoinToGame", "status": "ok"}
-                await websocket.send_json(payload)
-            else:
-                print("game is not exist")
-        elif data["method"] == "HandleMove":
-            row, col = data.get("row"), data.get("column")
-            payload = {"method": "HandleMove", "status": "ok", "has_winner": False}
+    try:
+        while True:
+            data = await websocket.receive_json()
 
-            if row is not None and col is not None:
-                print(player.id)
-                game.play(row, col)
-
-                # send this move to the next player
-                # in play() method the toggle_player() method is called
-                player_id = game.current_player.id
-                player_ws: WebSocket = players.get(player_id)
-                print(player_id)
-                if player_ws:
-                    move = f"{row}_{col}"
-                    payload["message"] = move
-                    await player_ws.send_json(payload)
-
-                # if the game hss winner send winner compos to both players
-                if game.has_winner():
-                    for player in game.players:
-                        player_ws: WebSocket = players.get(player.id)
-                        payload["winner_combo"] = game.winner_combo
-                        payload["has_winner"] = True
-                        payload["winner_player"] = game._winner_player
-                        await player_ws.send_json(payload)
-            else:
-                print(row, col)
-        elif data["method"] == "JoinToRandomGame":
-            player.label = "O"
-            payload = {
-                "method": "JoinToRandomGame",
-            }
-            player_ws: WebSocket = players.get(player.id)
-            if len(games) > 0:
-                game = random.choice(list(games.values()))
-                game.add_player(player)
-                payload["message"] = "You have joined the game successfully!"
-                payload["status"] = "ok"
-                await player_ws.send_json(payload)
-            else:
-                payload["message"] = "There is no any game!"
-                payload["status"] = "NotFound"
-                await player_ws.send_json(payload)
+            if data["method"] == "CreateGame":
+                game = TicTacToeGame(players=(player,))
+                games[game._game_id] = game
+                await create_game(game, manager, websocket)
+            elif data["method"] == "JoinToGame":
+                game_id = data.get("gameId")
+                game: TicTacToeGame = games.get(game_id)
+                await join_game(player, game, manager, websocket)
+            elif data["method"] == "HandleMove":
+                row, col = data.get("row"), data.get("column")
+                game_id = data.get("gameID")
+                game = games.get(game_id)
+                await handle_move(row, col, game, manager)
+            elif data["method"] == "JoinToRandomGame":
+                if len(games) > 0:
+                    game = random.choice(list(games.values()))
+                else:
+                    game = None
+                await join_random_game(game, player, manager, websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(player.id)
